e838f4f8842c47e48f36de02623b136da6cdc1ad
rorounifix@gmail.com <rorounifix@gmail.com>
2017-07-09 16:19:22 +0300

    add comments

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from kivy.uix.gridlayout import GridLayout
 
 
 class CalcGridLayout(GridLayout):
+
+    # Calculate the input numbers
+
     def calC(self, calculate):
         if calculate:
             try:
                 self.display.text = str(eval(calculate))
             except:
+                #comes an error if wrong syntax
                 self.display.text = 'Error'
 
     def clear(self):
+        # When click the AC button it clear the display
         self.display.text = ''
 
 
