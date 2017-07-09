ccfac14a7b30e678de2ed04117ce73f324f0313c
rorounifix@gmail.com <rorounifix@gmail.com>
2017-07-09 14:59:18 +0300

    update for the eval() calculation

---
+++ main.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.gridlayout import GridLayout
+
+
+class CalcGridLayout(GridLayout):
+    def calC(self, calculate):
+        if calculate:
+            try:
+                self.display.text = str(eval(calculate))
+            except:
+                self.display.text = 'Error'
+
+
+class MainApp(App):
+
+    def build(self):
+        return CalcGridLayout()
+
+
+if __name__ == "__main__":
+    MainApp().run()
