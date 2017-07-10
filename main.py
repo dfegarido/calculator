8cc3281601f020ca8451cdd8193f71f48747d0af
rorounifix@gmail.com <rorounifix@gmail.com>
2017-07-10 16:06:16 +0300

    update

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 
+
+
+test_x = []
+
 class CalcGridLayout(GridLayout):
 
-    # Calculate the input numbers
+     # Calculate the input numbers
 
     def calC(self, calculate):
         if calculate:
@@ -18,12 +22,34 @@ class CalcGridLayout(GridLayout):
         # When click the AC button it clear the display
         self.display.text = ''
 
-class AddMemory(CalcGridLayout):
-    
 
+    # add the sum to local memory
 
-class SubMemory():
-    pass
+    def add_mem(self, positive):
+        global test_x
+        if positive:
+            try:
+                test_x.append(float('+{}'.format(positive)))
+                print(test_x)
+                self.display.text = ''
+            except:
+                self.display.text = "Error"
+
+    #subtract the sum from the local memory
+    def sub_mem(self, negative):
+        global test_x
+        test_x.append(float('-{}'.format(negative)))
+        print(test_x)
+        self.display.text = ''
+
+    def mrc_mem(self):
+        try:
+            self.display.text = str(sum(test_x))
+        except:
+            self.display.text = "Error"
+
+        return True
+
 
 
 class MainApp(App):
